ff7622f3cc4ee630ef978789e4cb30e9240a8132
pgjon <jones.rgv@gmail.com>
2025-08-29 22:34:00 -0300

    aula 01

--- Semana-1/projetoTeste/src/Main.java
+++ Semana-1/projetoTeste/src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
 
-public class main {
+        // Tipos primitivos ( Tipos básicos )
+        // Inteiro
+        // Tipo + nomeDaVariável = valor
+        int inteiro = 1_000_000;
+        float numeroComVirgula = 15.0f;
+        double numeroComVirgula2 = 15.0;
+
+        // caracteres
+        char a = 'a';
+
+        // booleano
+        boolean b = true;
+    }
 }
